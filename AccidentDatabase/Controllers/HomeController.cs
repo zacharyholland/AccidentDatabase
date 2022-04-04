9335948c29f0aafdd54d07824848dd1fa1d0be01
bradfordhuber <bradfordjhuber@gmail.com>
2022-04-04 14:49:54 -0600

    commit

--- AccidentDatabase/Controllers/HomeController.cs
+++ AccidentDatabase/Controllers/HomeController.cs
@@ -5,25 +5,30 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using AccidentDatabase.Models;
 
 namespace AccidentDatabase.Controllers
 {
     public class HomeController : Controller
     {
-        private IAccidentRepository repo { get; set; }
-
-        public HomeController(IAccidentRepository temp)
-        {
-            repo = temp;
-        }
 
         public IActionResult Index()
         {
-            var x = repo.Accidents
-                .OrderBy(a => a.CRASH_ID)
-                .ToList();
 
+
+            return View();
+        }
+
+        public IActionResult Accidents()
+        {
+            //var x = repo.Accidents
+            //    .OrderBy(a => a.CRASH_ID)
+            //    .ToList();
+
+            return View();
+        }
+
+        public IActionResult Data()
+        {
             return View();
         }
     }
